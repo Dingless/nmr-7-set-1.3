a64d85dec1b2b9db64691214aa58cd999888d7bd
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-01-31 12:46:19 +0100

    nmr 7

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿class program
+{
+    static void Main()
+    {
+        string input; // variabel för att lagra det användaren har skrivit
+        int number; // variabel för konvertera heltalet
+        bool success = false; // håller reda om konvertering lyckades ele inte
+
+        while (!success) // en while loop som kommer fortsätta sucess är falkst
+        {
+            Console.WriteLine(" Ange en siffra");
+            input = Console.ReadLine();
+            success = int.TryParse(input, out number); // försök konvertera inmatningen till ett heltal
+            if (!success) // ifall konvertering misslyckades så skrivs det ut ett fel meddelande
+            {
+                Console.WriteLine("Felaktig inmatning ");
+
+            }
+
+        Console.WriteLine("Du angav ett heltal:" + number); // skriv ut det angivna heltalet när konvertering lyckas
+        }
+
+
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
